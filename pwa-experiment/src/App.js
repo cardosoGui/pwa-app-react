f0630fb23b5fa7bbb281429c61626a3fefc5b962
Guilherme Cardoso de Almeida <igui.trb20@outlook.com>
2018-06-05 22:42:20 -0300

    UI components

--- pwa-experiment/src/App.js
+++ pwa-experiment/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Router, browserHistory, Route, Link } from "react-router";
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 const Page = ({ title }) => (
   <div className="App">
     <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>{title}</h2>
+      <Header title="NavBar" />
     </div>
     <p className="App-intro">This is the {title} page.</p>
     <p>
@@ -22,7 +21,7 @@ const Page = ({ title }) => (
   </div>
 );
 
-const Home = props => <Page title="Home" />;
+const Home = props => <Page title="Guilherme" />;
 
 const About = props => <Page title="About" />;
 
